940c3df045a5b5020bde06fb1e21ce47104cc130
R. Kukuh <rkukuh@gmail.com>
2018-07-27 00:30:20 +0700

    Add solution to Distinct lesson

---
+++ src/com/rkukuh/codility/_06/distinct/Test.java
@@ -0,0 +1,13 @@
+package com.rkukuh.codility._06.distinct;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        int[] A = {2, 1, 1, 2, 3, 1};
+
+        Distinct d = new Distinct();
+
+        System.out.println(d.solution(A)); // 3
+    }
+}
