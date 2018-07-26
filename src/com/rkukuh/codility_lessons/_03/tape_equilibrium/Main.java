8f7ab7d45c36ad46032e6e16559a8f4579a12831
R. Kukuh <rkukuh@gmail.com>
2018-07-26 16:03:02 +0700

    Add solution for TapeEquilibrium lesson

---
+++ src/com/rkukuh/codility_lessons/_03/tape_equilibrium/Main.java
@@ -0,0 +1,13 @@
+package com.rkukuh.codility_lessons._03.tape_equilibrium;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        TapeEquilibrium te = new TapeEquilibrium();
+
+        int[] A = {3, 1, 2, 4, 3};
+
+        System.out.println(te.solution(A)); // 1
+    }
+}
