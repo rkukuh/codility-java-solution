0ceda2d05e84619598013b0f5ed5495f4c78a2db
R. Kukuh <rkukuh@gmail.com>
2018-07-26 16:46:19 +0700

    Add solution for FrogRiverOne lesson

---
+++ src/com/rkukuh/codility_lessons/_04/frog_river/Main.java
@@ -0,0 +1,22 @@
+package com.rkukuh.codility_lessons._04.frog_river;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] A = new int[10];
+
+        A[0] = 1;
+        A[1] = 3;
+        A[2] = 1;
+        A[3] = 4;
+        A[4] = 2;
+        A[5] = 3;
+        A[6] = 5;
+        A[7] = 4;
+
+        FrogRiverOne frog = new FrogRiverOne();
+
+        System.out.println(frog.solution(5, A)); // 6
+    }
+}
