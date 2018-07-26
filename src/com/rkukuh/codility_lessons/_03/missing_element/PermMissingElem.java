253d8c62bbdf46ce7919843196e5cdd3f0f9de08
R. Kukuh <rkukuh@gmail.com>
2018-07-26 15:51:41 +0700

    Add alternative solution for performance matter

--- src/com/rkukuh/codility_lessons/_03/missing_element/PermMissingElem.java
+++ src/com/rkukuh/codility_lessons/_03/missing_element/PermMissingElem.java
@@ -1,18 +1,38 @@
 package com.rkukuh.codility_lessons._03.missing_element;
 
+import java.util.HashSet;
+
 public class PermMissingElem
 {
-    public int solution(int[] A) {
+    public int solution(int[] A)
+    {
+        /** Solution #2: PERFORMANCE */
 
-        int N = A.length + 1;
+        int missing = 1;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int a : A) {
+            set.add(a);
+        }
+
+        while (set.contains(missing)) {
+            missing++;
+        }
+
+        return missing;
+
+
+        /** Solution #1: SIMPLE */
+
+        /*int N = A.length + 1;
 
         int missing = ((N + 1) * N) / 2;
 
         for (int x : A) {
-
             missing -= x;
         }
 
-        return missing;
+        return missing;*/
     }
 }
