cfd881455f2b548655eb79ff7372c226981ece20
R. Kukuh <rkukuh@gmail.com>
2018-07-27 05:54:39 +0700

    FIX: Int overflow error

--- src/com/rkukuh/codility/_06/triangle/Triangle.java
+++ src/com/rkukuh/codility/_06/triangle/Triangle.java
@@ -6,16 +6,11 @@ public class Triangle
 {
     public int solution(int[] A)
     {
-        if (A.length < 3) {
-
-            throw new IllegalArgumentException("Invalid input");
-        }
-
         Arrays.sort(A);
 
         for (int i = 0; i < (A.length - 2); i++) {
 
-            if (A[i] + A[i + 1] > A[i + 2]) {
+            if ((long) A[i] + (long) A[i + 1] > (long) A[i + 2]) {
 
                 return 1;
             }
