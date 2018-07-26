f53ba3cae06dccb1398e77ea5b6f02c99ee897fb
R. Kukuh <rkukuh@gmail.com>
2018-07-26 21:02:49 +0700

    Add solution for MissingInteger lesson

---
+++ src/com/rkukuh/codility_lessons/_04/missing_integer/MissingInteger.java
@@ -0,0 +1,11 @@
+package com.rkukuh.codility_lessons._04.missing_integer;
+
+class MissingInteger
+{
+    public int solution(int[] A)
+    {
+        // The solution is exactly the same as Lesson 03: PermMissingElem
+
+        return 0;
+    }
+}
