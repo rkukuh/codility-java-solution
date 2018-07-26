e27313f4f58d138626859dfd6ab00731544614d3
R. Kukuh <rkukuh@gmail.com>
2018-07-26 13:58:43 +0700

    Add binary gap lesson answer

---
+++ src/com/rkukuh/Main.java
@@ -0,0 +1,9 @@
+package com.rkukuh;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	    System.out.println("Welcome to Codility Lessons answer");
+    }
+}
---
+++ src/com/rkukuh/codilitylessons/binarygap/Main.java
@@ -0,0 +1,14 @@
+package com.rkukuh.codilitylessons.binarygap;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        BinaryGap bg = new BinaryGap();
+
+        //int test = 9;       // 2
+        int test = 1041;    // 5
+
+        System.out.println(bg.solution(test));
+    }
+}
