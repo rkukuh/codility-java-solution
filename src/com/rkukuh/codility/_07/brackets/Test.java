3e1c8df67c370451a58db01895d2161e285423ed
R. Kukuh <rkukuh@gmail.com>
2018-07-27 07:54:59 +0700

    Add solution for Brackets lesson

---
+++ src/com/rkukuh/codility/_07/brackets/Test.java
@@ -0,0 +1,15 @@
+package com.rkukuh.codility._07.brackets;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        String S1 = "{[()()]}";
+        String S2 = "([)()]";
+
+        Brackets b = new Brackets();
+
+        System.out.println(b.solution(S1)); // 1
+        System.out.println(b.solution(S2)); // 0
+    }
+}
