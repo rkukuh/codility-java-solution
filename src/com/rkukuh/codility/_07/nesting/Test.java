9663b0f500a4ea81795d0752d4ba963882e0c445
R. Kukuh <rkukuh@gmail.com>
2018-07-27 07:43:02 +0700

    Add solution for Nesting lesson

---
+++ src/com/rkukuh/codility/_07/nesting/Test.java
@@ -0,0 +1,15 @@
+package com.rkukuh.codility._07.nesting;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        String S1 = "(()(())())";
+        String S2 = "())";
+
+        Nesting n = new Nesting();
+
+        System.out.println(n.solution(S1)); // 1
+        System.out.println(n.solution(S2)); // 0
+    }
+}
