74c89dd2ca604f771315deca9c9c337627851cf8
R. Kukuh <rkukuh@gmail.com>
2018-07-28 22:46:35 +0700

    Add solution for EquiLeader lesson

---
+++ src/com/rkukuh/codility/_08/equi_leader/EquiLeader.java
@@ -0,0 +1,58 @@
+package com.rkukuh.codility._08.equi_leader;
+
+public class EquiLeader
+{
+    public int solution(int[] A)
+    {
+        int size = 0;
+        int value = 0;
+
+        for (int aA : A) {
+
+            if (size == 0) {
+
+                size++;
+                value = aA;
+            }
+            else {
+
+                if (aA == value) size++;
+                else size--;
+            }
+        }
+
+
+        int count = 0;
+
+        for (int aA : A) {
+
+            if (aA == value) count++;
+        }
+
+        if (count <= (A.length/2)) {
+
+            return 0;
+        }
+        else {
+
+            int indices = 0;
+            size = 0;
+
+            for (int k=0; k<A.length-1; k++) {
+
+                if (A[k] == value) {
+
+                    count--;
+                    size++;
+                }
+
+                if ((size > ((k+1)/2)) && (count > (A.length-(k+1))/2 )) {
+
+                    indices++;
+                }
+            }
+
+            return indices;
+        }
+    }
+}
---
+++ src/com/rkukuh/codility/_08/equi_leader/Test.java
@@ -0,0 +1,13 @@
+package com.rkukuh.codility._08.equi_leader;
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        int[] A = {4, 3, 4, 4, 4, 2};
+
+        EquiLeader el = new EquiLeader();
+
+        System.out.println(el.solution(A)); // 2~
+    }
+}
